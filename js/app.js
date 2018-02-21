3229b0c89f19df82bb94aae2578c0f83ec1acbef
lastnamearya <arya.jigyasu6815@gmail.com>
2018-02-21 19:15:55 +0530

    get element by id

---
+++ js/app.js
@@ -0,0 +1,5 @@
+// Select the Element and store it in a variable
+var el = document.getElementById('one');
+
+// Change the Value of the class attribute
+el.className = 'cool';
--- js/get-element-by-id.js
+++
@@ -1,3 +0,0 @@
-var el = document.getElementById('one');
-
-el.className = 'cool';
