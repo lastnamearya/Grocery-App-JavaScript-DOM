eb8fd180ed84adb123764d22e0845848e03a574a
lastnamearya <arya.jigyasu6815@gmail.com>
2018-02-21 21:34:09 +0530

    looping through NodeList

--- js/app.js
+++ js/app.js
@@ -1,10 +1,7 @@
-// querySelector() only returns the first match
-var el = document.querySelector('li.hot');
-el.className = 'cool';
+var hotItems = document.querySelectorAll('li.hot');
 
-// querySelector() returns a NodeList
-var els = document.querySelectorAll('li.hot');
-
-// The second matching element (the third list item) is selected and changed, js executes statements line by line so what happen here is first li item during executing becomes 'cool'
-
-els[1].className = 'cool';
+if(hotItems.length > 0){
+  for(var i = 0; i < hotItems.length; i++){
+    hotItems[i].className = 'cool';
+  }
+}
