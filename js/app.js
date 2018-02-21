7944a08bc2028bea4b46f8f6e0a7240a09bfd3cc
lastnamearya <arya.jigyasu6815@gmail.com>
2018-02-21 22:00:27 +0530

    child

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
-var startItem = document.getElementById('two');
-var prevItem = startItem.previousElementSibling;
-var nextItem = startItem.nextElementSibling;
+// It will give us an array of "ul" tags or whatever the tag it is, it will always gives us an array
 
-prevItem.className = 'complete';
-nextItem.className = 'cool';
+// var startItem = document.getElementsByTagName('ul');
+
+// Now we have to selece the first item in the array, if there is only one item, then still we get an array with 0 length.
+
+var startItem = document.getElementsByTagName('ul')[0];
+var firstItem = startItem.firstElementChild;
+var lastItem = startItem.lastElementChild;
+
+// Change the Value of the children's class attributes
+firstItem.setAttribute('class', 'complete');
+lastItem.setAttribute('class', 'cool');
