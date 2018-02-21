4c80e970fae04a0954ff72fdd31f755109b6bf2a
lastnamearya <arya.jigyasu6815@gmail.com>
2018-02-22 00:12:17 +0530

    inner html

--- js/app.js
+++ js/app.js
@@ -1,13 +1,8 @@
-// It will give us an array of "ul" tags or whatever the tag it is, it will always gives us an array
+// Store the first list item in a variable
+var firstItem = document.getElementById('one');
 
-// var startItem = document.getElementsByTagName('ul');
+// Get the content of the first list item
+var itemContent = firstItem.innerHTML;
 
-// Now we have to selece the first item in the array, if there is only one item, then still we get an array with 0 length.
-
-var startItem = document.getElementsByTagName('ul')[0];
-var firstItem = startItem.firstElementChild;
-var lastItem = startItem.lastElementChild;
-
-// Change the Value of the children's class attributes
-firstItem.setAttribute('class', 'complete');
-lastItem.setAttribute('class', 'cool');
+// Update the content of the first list item so it is a link
+firstItem.innerHTML = '<a href=\"http://example.org\">' + itemContent + '</a>';
