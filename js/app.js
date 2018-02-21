86ce68d34f8ac5d2404ca7866efdde1f0b850eac
lastnamearya <arya.jigyasu6815@gmail.com>
2018-02-22 03:35:35 +0530

    remove-attribute

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 var firstItem = document.getElementById('one');
-firstItem.className = 'complete';
-
-var fourthItem = document.getElementById('four');
-fourthItem.setAttribute('class', 'cool');
+if (firstItem.hasAttribute('class')) {
+  firstItem.removeAttribute('class');
+}
